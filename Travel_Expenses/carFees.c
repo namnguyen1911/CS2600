d587ea22d2f7df27004a93dfe70a2c33de096d3b
Jesse Chu <jwchu@cpp.edu>
2021-09-25 19:41:04 -0700

    Add new file carFees.c

---
+++ Travel_Expenses/carFees.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+double calcCostOfMiles (int miles){
+    return miles * 0.27;
+}
+
+double calcCostOfEmployeeParkingFees (int daysOfTrip, int fees){
+    // NOTE: WIP
+    return 0.0;
+}
+
+double calcCostOFCompanyParkingFees (int daysOfTrip, int fees){
+    // NOTE: WIP
+    return 0.0;
+}
+
+double calcCostOfEmployeeTaxiFees (int daysOfTrip, int fees){
+    // NOTE: WIP
+    return 0.0;
+}
+
+double calcCostOfCompanyParkingFees (int daysOfTrip, int fees){
+    // NOTE: WIP
+    return 0.0;
+}
