f38f4c99c4085938227780d3d12883730fe897e3
YvetteCarlos <89424585+YvetteCarlos@users.noreply.github.com>
2021-09-26 13:40:43 -0700

    Delete totalExpenses.c

--- totalExpenses.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "header.h"
-
-double totalExpense(int totalEmployee){
-    totalEmployee =  airfare + 0.27*miles + parkingFees + taxiFees + conferenceFees;
-    return 0;
-}
-
